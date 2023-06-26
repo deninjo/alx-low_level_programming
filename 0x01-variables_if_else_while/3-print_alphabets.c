23ecc5ee669bc45f8c732ccddc9d8e98262f8dca
deninjo <117518718+deninjo@users.noreply.github.com>
2023-06-27 00:25:14 +0300

    Updated 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,6 @@ int main(void)
 {
 char c;
 
-<<<<<<< HEAD
 for (c = 'a'; c <= 'z'; c++)
 {
     putchar(c);
@@ -19,18 +18,6 @@ for (c = 'A'; c <= 'Z'; c++)
     putchar(c);
 }
     putchar('\n');
-=======
-	char c, C;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	for (C = 'A'; C <= 'Z'; C++)
-	{
-		putchar(C);
-	}
-	putchar('\n');
->>>>>>> d65050328da06a60df6adc2e0cbee98bfea8fced
 return (0);
 }
