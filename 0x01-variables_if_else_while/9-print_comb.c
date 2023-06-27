76dd22ca10dc5e04246e37cdda56f012fdb4f8e7
deninjo <imwendwadennis@gmail.com>
2023-06-27 01:59:11 -0700

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers separated by ',' using putchar
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+
+return (0);
+}
